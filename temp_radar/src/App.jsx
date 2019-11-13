29940ba1aec67fd3a109132eac9e68061f8d8b30
Flaman <flamandzki.artur@gmail.com>
2019-11-13 11:23:10 +0100

    Clean

---
+++ temp_radar/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => (
+    <div>
+      <div className="data">
+        <strong>Time</strong>: 07:00
+      </div>
+      <div className="data">
+        <strong>Time</strong>: 15:00
+      </div>
+    </div>
+  );
+
+export default App;
